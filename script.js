5bae5e5a96223f0b1df1945a59467fdef049e87d
Ubeydu <u.keles@gmail.com>
2023-09-27 22:38:08 +0300

    create life object

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Welcome to random life.
 
+const life = {
+    countries: ['Sudan', 'Turkey', 'New Zealand', 'Chile'],
+    jobs: ['plummer', 'painter', 'business person', 'olive grower'],
+    diet: ['vegetarian', 'omnivore', 'vegan'],
+};
 
-let countries = ['Sudan', 'Turkey', 'New Zealand', 'Chile'];
-let jobs = ['plummer', 'painter', 'business person', 'olive grower'];
 
-console.log(`you live in ${countries[i]} and you are a/an ${jobs[j]}.`);
+
+console.log(`you live in ${countries[i]} and you are a/an ${jobs[j]}. And, finally, yout diet is ${diet[k]}`);
 
